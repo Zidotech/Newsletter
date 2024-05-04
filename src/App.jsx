bf06932b2c7f86033bfe670bbbc321e782248f22
Zulu <zulu.devs@gmail.com>
2024-05-04 16:12:46 +0100

    newsletter

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MailchimpForm from "./components/MailchimpForm";
+
+function App() {
+  return (
+    <div>
+      <MailchimpForm />
+    </div>
+  );
+}
+
+export default App;
